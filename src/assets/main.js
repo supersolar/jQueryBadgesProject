09d6f462468e68eed14b938a3f69bc3bf9524e82
Kirk Solar <ksolar@sitecrafting.com>
2017-08-12 23:05:57 -0700

    adding ajax call for codeschool completed course data and output.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/kirksolar.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+	  var output = '';
+	  var len;
+	  var i;
+		for (i = 0, len = response.courses.completed.length; i < len; i++) {
+			output += '<div class="course">'
+				+'<h3>'+response.courses.completed[i]['title']+'</h3>'
+				+'<img src="'+response.courses.completed[i]['badge']+'"/>'
+				+'<a href="'+response.courses.completed[i]['url']+'" target="_blank" class="btn btn-primary">See Course</a>'
+			+'</div>';
+		}
+	  $('#badges').html(output);
+	  console.log(response.courses);
+    }
+  });
 
 });
